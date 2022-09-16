40fdd488ce8a55526637d6bcee30e2b33621c8d0
Seek <54331826+iSeeeeker@users.noreply.github.com>
2022-09-16 12:44:27 +0200

    Update youtube_download.py

--- youtube_download.py
+++ youtube_download.py
@@ -6,7 +6,7 @@ root.geometry('600x500')
 root.resizable(0,0)
 root.title("Youtube Video Downloader")
 Label(root,text = 'Youtube Video Downloader', font ='arial 20 bold').pack()
-##enter link
+#enter link
 link = StringVar()
 Label(root, text = 'Paste Link Here:', font = 'arial 15 bold').place(x= 160 , y = 60)
 link_enter = Entry(root, width = 50,textvariable = link).place(x = 32, y = 90)
@@ -28,3 +28,5 @@ def Downloadermp3():
 Button(root,text = 'DOWNLOAD IN MP3 FORMAT', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = Downloadermp3).place(x=120 ,y = 150)
 Button(root,text = 'DOWNLOAD IN MP4 FORMAT', font = 'arial 15 bold' ,bg = 'pale violet red', padx = 2, command = Downloadermp4).place(x=120 ,y = 200)
 root.mainloop()
+
+#Can download both mp3 and mp4 format. Just past the link and click "mp3" or "mp4"
